806d6743a8fe8218e16f889a499a8b96f834e432
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:14:12 +0300

    json fnished lab 2

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,12 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName: "Noor",
+	lastName: "Diab",
+	favoriteFoods: ["pizza", "chiken", "meat"],
+	age: 17,
+	picture: "https://www.google.co.il/search?client=ubuntu&hs=nQC&biw=1299&bih=647&tbm=isch&sa=1&ei=_pJhW-WyHsnbwQLo97nQAg&q=arsenal&oq=arsenal&gs_l=img.3..0l10.28636.29680.0.29863.7.5.0.2.2.0.125.575.0j5.5.0....0...1c.1.64.img..0.7.588...0i67k1.0.jXW4PpU1dVE#imgrc=IY7y-tr6IfwpcM:"
+	
+
+}
+var string = JSON.stringify(me);
+document.write(string);
